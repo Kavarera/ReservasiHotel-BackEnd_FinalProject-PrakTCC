13b20ca8e76246b887c05ac511bd403eee0cd5b0
Kavarera <rafli.030715@gmail.com>
2024-05-22 18:27:36 +0700

    change connection.js

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.DATABASE
     ,process.env.USER
     ,process.env.PASSWORD,{
         host: process.env.HOST,
-        dialect: process.env.DIALECT
+        dialect: "mysql"
     });
 
 module.exports = sequelize;
