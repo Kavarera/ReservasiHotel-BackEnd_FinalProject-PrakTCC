bdd9f847b3fc579c902437d9ca439b8a7b628ed3
Kavarera <rafli.030715@gmail.com>
2024-05-22 19:50:56 +0700

    add documentation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const sequelize = require('./src/utils/connection');
 const Association = require('./src/utils/associations');
+const path = require('path');
 
 const app = express();
 app.use(function (req, res, next) {
@@ -10,10 +11,15 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use(express.json());
 const route = require('./src/routes/route');
 app.use('/api', route);
+
+app.use('/',express.Router().get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+}));
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,16 +29,3 @@ const PORT = process.env.PORT || 3000;
   }).catch(e=>{
     console.log(`ERROR : ${e.message}`);
   });
-
-  // sequelize.sync({ force: false }).then(() => {
-  //   app.listen(PORT, () => {
-  //     console.log(`Server is running on port ${PORT}`);
-  //   });
-  // }).catch(error => {
-  //   console.error('Unable to connect to the database:\n', error);
-  // });
-
-
-  // app.listen(PORT,()=>{
-  //   console.log(`server is running on port ${PORT}\nhttp://${process.env.HOST}:${process.env.PORT}`);
-  // });
